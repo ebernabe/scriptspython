76f43163b76f5f3b217312dd001cdaffac49de54
Ernesto Bernabe <ernes.bs@gmail.com>
2012-01-13 08:42:49 -0800

    axx

---
+++ axx.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+#Filename: axx.py
+
+import os
+import sys
+import time
+
+def replace_all(text, dic):
+    for i, j in dic.iteritems():
+        text = text.replace(i, j)
+    return text
+
+
+dir_dns = raw_input("Digite la direccion del archivo: ")
+dns = open(dir_dns)
+archivo = dns.read()
+
+print archivo
+
+serial1 = raw_input("Digite el serial: ")
+#serial2 = raw_input("nuevo el serial: ")
+
+if time.strftime("%Y%m%d") == serial1[:8]:
+      serial2 = str(int(serial1)+1)
+else:
+      serial2 = time.strftime("%Y%m%d")+"01"
+
+resultado = replace_all(archivo, {"174.129.214.228":"184.72.58.15",serial1:serial2})
+
+dns.close()
+
+dnswrite = open(dir_dns,"w")
+dnswrite.write(resultado)
+dnswrite.close()
+
+
+print resultado
